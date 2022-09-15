c2920a2f7a973eac97c71ce903dd5f1fd2cefd71
Chef Karelian <78794805+karelianpie@users.noreply.github.com>
2022-09-16 00:30:48 +1000

    feat: Add support for multiple subgraph urls (#119)
    * feat: Add support for multiple subgraph urls
    
    * refactor: Add subgraph endpoints
    
    * style: Indentation
    
    * style: Indentation part II

--- components/StandardFooter.tsx
+++ components/StandardFooter.tsx
@@ -1,19 +1,27 @@
 import React, {ReactElement} from 'react';
 import {SwitchTheme} from '@yearn-finance/web-lib/components';
-import {useUI} from '@yearn-finance/web-lib/contexts';
+import {useUI, useWeb3} from '@yearn-finance/web-lib/contexts';
 import meta from 'public/manifest.json';
+
+const SUBGRAPH_URLS = new Map([
+	[1, 'https://api.thegraph.com/subgraphs/name/rareweasel/yearn-vaults-v2-subgraph-mainnet'],
+	[10, 'https://api.thegraph.com/subgraphs/name/yearn/yearn-vaults-v2-optimism'],
+	[250, 'https://api.thegraph.com/subgraphs/name/bsamuels453/yearn-fantom-validation-grafted'],
+	[42161, 'https://api.thegraph.com/subgraphs/name/yearn/yearn-vaults-v2-arbitrum']
+]);
 
 function	Footer(): ReactElement {
 	const	{theme, switchTheme} = useUI();
+	const	{chainID} = useWeb3();
 
 	return (
 		<footer className={'mx-auto mt-auto hidden w-full max-w-6xl flex-row items-center py-8 md:flex'}>
 			<a href={meta.github} target={'_blank'} className={'pr-6 text-xs text-neutral-500 transition-colors hover:text-accent-500 hover:underline'} rel={'noreferrer'}>
 				{'Yearn.watch repo'}
 			</a>
-			<a href={'https://gov.yearn.finance/'} target={'_blank'} className={'pr-6 text-xs text-neutral-500 transition-colors hover:text-accent-500 hover:underline'} rel={'noreferrer'}>
+			{SUBGRAPH_URLS.has(chainID) && <a href={SUBGRAPH_URLS.get(chainID)} target={'_blank'} className={'pr-6 text-xs text-neutral-500 transition-colors hover:text-accent-500 hover:underline'} rel={'noreferrer'}>
 				{'Subgraph'}
-			</a>
+			</a>}
 			<a href={'https://discord.yearn.finance/'} target={'_blank'} className={'pr-6 text-xs text-neutral-500 transition-colors hover:text-accent-500 hover:underline'} rel={'noreferrer'}>
 				{'Discord'}
 			</a>
