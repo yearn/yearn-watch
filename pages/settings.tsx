765e43e9565cd391e30e8b35544d51e046f775ce
ai's slave <6696080+ai-slave@users.noreply.github.com>
2022-12-14 10:28:10 +0000

    Improved settings page, switched from using env variables to settings (#134)
    * Improved settings page, switched from using env variables to settings
    
    Fixes #133
    
    * Design cleanup, removed Lens
    
    * Remove subgraph and explorer settings
    
    * removed caption, linted
    
    * variable names fix
    
    * settings call moved out of hook
    
    Co-authored-by: Major <90963895+Majorfi@users.noreply.github.com>

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,6 +1,136 @@
-import React, {ReactElement} from 'react';
-import {Card, Switch} from '@yearn-finance/web-lib/components';
-import {useSettings} from 'contexts/useSettings';
+import React, {ReactElement, useMemo, useState} from 'react';
+import {Button, Card, Switch} from '@yearn-finance/web-lib/components';
+import {useSettings as productUseSettings} from 'contexts/useSettings';
+import {useSettings} from '@yearn-finance/web-lib/contexts';
+
+type TWrappedInput = {
+	title: string;
+	initialValue: string;
+	onSave: (value: string) => void;
+}
+
+function	WrappedInput({title, initialValue, onSave}: TWrappedInput): ReactElement {
+	const	[isFocused, set_isFocused] = useState(false);
+	const	[value, set_value] = useState(initialValue);
+	const	isInitialValue = useMemo((): boolean => value === initialValue, [value, initialValue]);
+
+	return (
+		<label>
+			<p className={'pb-1 text-neutral-900'}>{title}</p>
+			<div className={'flex flex-row space-x-2'}>
+				<div data-focused={isFocused} className={'yearn--input relative w-full'}>
+					<input
+						onFocus={(): void => set_isFocused(true)}
+						onBlur={(): void => set_isFocused(false)}
+						className={'h-10 w-full overflow-x-scroll border-2 border-neutral-700 bg-neutral-0 p-2 outline-none scrollbar-none'}
+						placeholder={'Use default'}
+						value={value}
+						type={'text'}
+						onChange={(e): void => set_value(e.target.value)}
+					/>
+				</div>
+				<Button
+					disabled={isInitialValue}
+					className={'w-full rounded-none md:w-48'}
+					onClick={(): void => onSave(value)}>
+					{'Save'}
+				</Button>
+			</div>
+		</label>
+	);
+}
+
+
+function	SectionRPCEndpoints(): ReactElement {
+	const	{onUpdateNetworks} = useSettings();
+	const	[, set_nonce] = useState(0);
+
+	return (
+		<Card>
+			<div className={'flex w-full flex-row justify-between pb-4'}>
+				<h4 className={'text-lg font-bold'}>{'RPC Endpoints'}</h4>
+			</div>
+			<div className={'text-justify'}>
+				<p>
+					{'The RPC Endpoints are the ways in which your wallet communicates with a given network. Without this, the dApp will not be able to read or write informations on the blockchain.'}
+				</p>
+				<div className={'mt-4 grid grid-cols-1 gap-4'}>
+					<WrappedInput
+						title={''}
+						initialValue={''}
+						onSave={(value): void => {
+							onUpdateNetworks({1: {rpcURI: value}});
+							set_nonce((n: number): number => n + 1);
+						}} />
+
+					<WrappedInput
+						title={''}
+						initialValue={''}
+						onSave={(value): void => {
+							onUpdateNetworks({10: {rpcURI: value}});
+							set_nonce((n: number): number => n + 1);
+						}} />
+
+					<WrappedInput
+						title={''}
+						initialValue={''}
+						onSave={(value): void => {
+							onUpdateNetworks({250: {rpcURI: value}});
+							set_nonce((n: number): number => n + 1);
+						}} />
+
+					<WrappedInput
+						title={''}
+						initialValue={''}
+						onSave={(value): void => {
+							onUpdateNetworks({42161: {rpcURI: value}});
+							set_nonce((n: number): number => n + 1);
+						}} />
+				</div>
+			</div>
+		</Card>
+	);
+}
+
+function	SectionYearnAPIBaseURI(): ReactElement {
+	const	{onUpdateBaseSettings, settings: baseAPISettings} = useSettings();
+
+	return (
+		<Card>
+			<div className={'flex w-full flex-row justify-between pb-4'}>
+				<h4 className={'text-lg font-bold'}>{'Yearn\'s APIs'}</h4>
+			</div>
+			<div className={'text-justify'}>
+				<p>
+					{'The Yearn\'s API endpoints are used to get some specific Yearn\'s related information about the vaults, the strategies and much more.'}
+				</p>
+				<div className={'mt-4 grid grid-cols-1 gap-4'}>
+					<WrappedInput
+						title={''}
+						initialValue={baseAPISettings.yDaemonBaseURI}
+						onSave={(value): void => onUpdateBaseSettings({
+							...baseAPISettings,
+							yDaemonBaseURI: value
+						})} />
+					<WrappedInput
+						title={''}
+						initialValue={baseAPISettings.apiBaseURI}
+						onSave={(value): void => onUpdateBaseSettings({
+							...baseAPISettings,
+							apiBaseURI: value
+						})} />
+					<WrappedInput
+						title={''}
+						initialValue={baseAPISettings.metaBaseURI}
+						onSave={(value): void => onUpdateBaseSettings({
+							...baseAPISettings,
+							metaBaseURI: value
+						})} />
+				</div>
+			</div>
+		</Card>
+	);
+}
 
 /* 🔵 - Yearn Finance **********************************************************
 ** Main render of the Settings page
@@ -11,7 +141,7 @@ function	Settings(): ReactElement {
 		shouldOnlyDisplayEndorsedVaults, switchShouldOnlyDisplayEndorsedVaults,
 		shouldDisplayVaultsWithMigration, switchShouldDisplayVaultsWithMigration,
 		shouldDisplayVaultNoStrats, switchShouldDisplayVaultNoStrats
-	} = useSettings();
+	} = productUseSettings();
 	/* 🔵 - Yearn Finance ******************************************************
 	** Main render of the page.
 	**************************************************************************/
@@ -60,7 +190,10 @@ function	Settings(): ReactElement {
 					</div>
 				</div>
 			</Card>
+			<SectionYearnAPIBaseURI />
+			<SectionRPCEndpoints />
 		</div>
+		
 	);
 }
 
